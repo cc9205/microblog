a048f2a68dd7e5c6c056561cfcaaa787d6397e14
cc9205 <cheng_cc@126.com>
2018-01-18 23:31:24 +0800

    templates01

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,20 @@
+from flask import render_template
 from app import apps
 
 @apps.route("/")
 @apps.route("/index")
 def index():
-    return "Hello,World!"
-
+    user = {'username': 'cc'}
+    posts = [
+        {
+            'author': {'username':'John'},
+            'body': 'Beautiful day in china!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home',user=user,posts=posts)
 if __name__ == '__main__':
     apps.run()

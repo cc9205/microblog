5a9217caa856f9430f7f2d3596f65b6bb0363748
cc9205 <cheng_cc@126.com>
2018-01-20 15:49:57 +0800

    login forms

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
-
+from config import Config
 apps = Flask(__name__)
+apps.config.from_object(Config)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
-from flask import render_template
+#coding=utf-8
+from flask import render_template, flash, redirect, url_for
 from app import apps
+from app.forms import LoginForm
 
 @apps.route("/")
 @apps.route("/index")
@@ -16,5 +18,15 @@ def index():
         }
     ]
     return render_template('index.html', title='Home',user=user,posts=posts)
+@apps.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for use {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+#        return redirect('/index')
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form) #左边的title和form指的是login.html里的变量，右边的form是上面的实例
+
+
 if __name__ == '__main__':
     apps.run()
